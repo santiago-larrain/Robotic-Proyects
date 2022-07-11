a7a53d3b151920e7c5b1489b262a490b12bd1dcf
Santiago Larrain <santiago.larrain@uc.cl>
2022-07-10 21:38:05 -0400

    PID implementado

--- Bored/Simulador de auto/objects.py
+++ Bored/Simulador de auto/objects.py
@@ -10,30 +10,60 @@ class Car(QObject):
         self.front = None
         self.back = None
         self.scale = None
+        self.ref_L = 0
+        self.ref_R = 0
         self.motor_L_RPM = 0
         self.motor_R_RPM = 0
+
+        self.motor_L_prev = 0
+        self.motor_R_prev = 0
+        self.error_L = 0
+        self.error_R = 0
+        self.error_L_prev = 0 
+        self.error_R_prev = 0
+        self.error_L_pprev = 0
+        self.error_R_pprev = 0
 
     def center_of_rotation(self):
         return self.back*(1 - self.p("gamma_1")) + self.front*self.p("gamma_1")
 
     def set_speed(self, command):
         if command == "w":
-            self.motor_L_RPM += 50
-            self.motor_R_RPM += 50
+            self.ref_L += 50
+            self.ref_R += 50
         elif command == "a":
-            self.motor_L_RPM -= 25
-            self.motor_R_RPM += 25
+            self.ref_L -= 25
+            self.ref_R += 25
         elif command == "s":
-            self.motor_L_RPM -= 50
-            self.motor_R_RPM -= 50
+            self.ref_L -= 50
+            self.ref_R -= 50
         elif command == "d":
-            self.motor_L_RPM += 25
-            self.motor_R_RPM -= 25
+            self.ref_L += 25
+            self.ref_R -= 25
         elif command == "stop":
-            self.motor_L_RPM = self.motor_R_RPM = 0
-        
+            self.ref_L = self.ref_R = 0
 
+    def PID(self, period):
+        k0 = self.p("kp")*(1 + self.p("ki")*period + self.p("kd")/period)
+        k1 = -self.p("kp")*(1 + 2*self.p("kd")/period)
+        k2 = self.p("kp")*self.p("kd")/period
+
+        self.error_L = (self.ref_L - self.motor_L_RPM)
+        self.error_R = (self.ref_R - self.motor_R_RPM)
+        self.motor_L_RPM = self.motor_L_prev + k0*self.error_L + k1*self.error_L_prev + k2*self.error_L_pprev
+        self.motor_R_RPM = self.motor_R_prev + k0*self.error_R + k1*self.error_R_prev + k2*self.error_R_pprev
+        
+        self.motor_L_prev = self.motor_L_RPM
+        self.motor_R_prev = self.motor_R_RPM
+        self.error_L_pprev = self.error_L_prev
+        self.error_R_pprev = self.error_R_prev
+        self.error_L_prev = self.error_L
+        self.error_R_prev = self.error_R
+
+        print(self.motor_L_RPM, self.ref_L)
+        
     def move(self, time):
+        self.PID(time)
         velocity = (self.motor_R_RPM + self.motor_L_RPM)/2 *self.p("WHEEL_RADIOUS") /60  # [m/s]
         angular_velocity = (self.motor_R_RPM - self.motor_L_RPM)*self.p("WHEEL_RADIOUS")/(self.p("CAR_WIDTH")/2) /60
         step = velocity * time
