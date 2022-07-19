fc8fce50d22d0d3464e91434b4c291226ec37003
Santiago Larrain <santiago.larrain@uc.cl>
2022-07-19 19:07:57 -0400

    New connection so the Task Manager has a lot of control

--- Bored/main_v1.py
+++ Bored/main_v1.py
@@ -20,22 +20,26 @@ if __name__ == "__main__":
     display = Display(app)
 
     # Create and add main car
-    robot = Car()
-    # Initial points
-    robot.back = np.array([display.p("WINDOW_WIDTH")//2 - 10, display.p("WINDOW_LENGHT")//2])
-    robot.front = np.array([display.p("WINDOW_WIDTH")//2, display.p("WINDOW_LENGHT")//2])
-    robot.scale = display.p("SCALE")
-    display.cars.append(robot)
+    scale = display.p("SCALE")
+
+    back = np.array([display.p("WINDOW_WIDTH")//2 - 10, display.p("WINDOW_LENGHT")//2])
+    front = np.array([display.p("WINDOW_WIDTH")//2, display.p("WINDOW_LENGHT")//2])
+    robot = Car(back, front, scale)
+
+    back = np.array([display.p("WINDOW_WIDTH")//2 - 10, display.p("WINDOW_LENGHT")//2])
+    front = np.array([display.p("WINDOW_WIDTH")//2, display.p("WINDOW_LENGHT")//2])
+    enemy = Car(back, front, scale)
 
     # Comms
     coms = SimComs()
 
     # Controller
     controller = Controller()
-    controller.car1 = robot
 
     # Task Manager
     task_manager = TaskManager()
+    task_manager.cars.append(robot)
+    task_manager.cars.append(enemy)
 
 
     # --- Connections ---
@@ -44,16 +48,16 @@ if __name__ == "__main__":
     display.keyboard_signal.connect(
         task_manager.manage_keyboard
     )
-    # End coms
-    display.end_signal.connect(
-        coms.end
-    )
     # Update message
     controller.update_message_signal.connect(
         coms.set_message
     )
 
     # --- Task Manager ---
+    # Add car
+    task_manager.add_car_signal.connect(
+        display.add_car
+    )
     # toggle automatic/manual drive
     task_manager.speed_controller_signal.connect(
         controller.toggle_controller
@@ -62,6 +66,10 @@ if __name__ == "__main__":
     task_manager.set_speed_signal.connect(
         coms.set_message
     )
+    # Toggle On/Off coms
+    task_manager.toggle_OnOff_coms_signal.connect(
+        coms.toggle_OnOff
+    )
 
     # --- Only for simulations ---
     coms.send_message_signal.connect(
@@ -69,11 +77,33 @@ if __name__ == "__main__":
     )
 
 
-    # --- Initialize program ---
-    coms.restart()
-    controller.thread.start()
-    display.thread.start()
+    # --- Start program ---
+    task_manager.restart_signal.connect(
+        controller.restart
+    )
+    task_manager.restart_signal.connect(
+        coms.restart
+    )
+    task_manager.restart_signal.connect(
+        robot.restart
+    )
 
+    # --- End program ---
+    task_manager.end_signal.connect(
+        controller.end
+    )
+    task_manager.end_signal.connect(
+        coms.end
+    )
+    task_manager.end_signal.connect(
+        robot.end
+    )
+    task_manager.end_signal.connect(
+        display.end
+    )
+
+    # --- Initialize program ---
+    display.restart()
 
     # --- Initialize QApp ---
     app.exec_()
