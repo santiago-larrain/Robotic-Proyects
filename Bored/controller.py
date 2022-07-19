4cd135a423ced2bbacbfc128864696d154bd419c
Santiago Larrain <santiago.larrain@uc.cl>
2022-07-19 16:57:52 -0400

    Test

--- Bored/controller.py
+++ Bored/controller.py
@@ -18,11 +18,27 @@ class Controller(QObject):
 
     def start(self):
         start = time.time()
-        while time.time() - start <= 2:
+        while time.time() - start <= 9.8:
             if self.control_speed:
-                self.update_message_signal.emit((1,1))
+                if time.time() - start <= 1:
+                    self.update_message_signal.emit((1,1))
+                elif time.time() - start <= 3:
+                    self.update_message_signal.emit((0.35,0.65))
+                elif time.time() - start <= 5:
+                    self.update_message_signal.emit((0.65,0.35))
+                elif time.time() - start <= 6:
+                    self.update_message_signal.emit((-2,-2))
+                elif time.time() - start <= 7:
+                    self.update_message_signal.emit((5,5))
+                elif time.time() - start <= 8:
+                    self.update_message_signal.emit((-1,-1))
+                elif time.time() - start <= 9:
+                    self.update_message_signal.emit((-1.1,-1))
+                else:
+                    self.update_message_signal.emit((-0.9,-1))
             time.sleep(0.1)
-        self.update_message_signal.emit((0,0))
+        if self.control_speed:
+            self.update_message_signal.emit((0,0))
     
     def toggle_controller(self, control):
         self.control_speed = control # bool
