a6e66aec479856e71ccea0f43cf0a4fdfa63b770
Santiago Larrain <santiago.larrain@uc.cl>
2023-09-06 15:33:20 -0300

    Mods 2

--- Laboratorio-Electronica-Analogica-Digital/Experiencia_1/Experiencia01_init/Experiencia01_init.srcs/sim_1/new/uart_tb.v
+++ Laboratorio-Electronica-Analogica-Digital/Experiencia_1/Experiencia01_init/Experiencia01_init.srcs/sim_1/new/uart_tb.v
@@ -1,4 +1,4 @@
-`timescale 100ps / 1ps
+`timescale 1ns / 1ns
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -25,115 +25,115 @@ module uart_tb;
     reg sw;
     reg rst;
     reg RsRx;
-    wire [15:0] voltage;
-    //wire rx_rdy;
-    //wire [7:0] rx_data;
+    wire [6:0] sseg_ca;
+    wire [3:0] sseg_an;
+    wire sseg_dp;
+    wire [8:0] LED;
 
-    top uut(clk, sw, rst, RsRx, voltage);
+    top uut(clk, sw, rst, RsRx, sseg_ca, sseg_an, sseg_dp, LED);
 
     initial begin 
         clk = 0;
-        forever clk = #(1) ~clk;
+        forever clk = #(5) ~clk;
     end
 
     initial begin 
     rst = 1;
     RsRx = 1;
     sw = 0;
-    #20 rst = 0;
-    #20;
+    #104167 rst = 0;
+    #104167 RsRx = 0;
+    
+    #104167 RsRx = 1;
+    #104167 RsRx = 1;
+    #104167 RsRx = 1;
+    #104167 RsRx = 0;
+    #104167 RsRx = 1;
+    #104167 RsRx = 1;
+    #104167 RsRx = 0;
+    #104167 RsRx = 0;
+    
+    #104167 RsRx = 1;
+    #208334;
     RsRx = 0;
     
-    #128 RsRx = 1;
-    #128 RsRx = 1;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    #128 RsRx = 1;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
+    #104167 RsRx = 1;
+    #104167 RsRx = 0;
+    #104167 RsRx = 0;
+    #104167 RsRx = 0;
+    #104167 RsRx = 0;
+    #104167 RsRx = 1;
+    #104167 RsRx = 0;
+    #104167 RsRx = 0;
     
-    #128 RsRx = 1;
-    #512;
-    RsRx = 0;
+    #104167 RsRx = 1;
+    #208334;
+//    RsRx = 0;
     
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 1;
+//    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 1;
+//    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 0;
     
-    #128 RsRx = 1;
-    #512;
-    RsRx = 0;
+//    #128 RsRx = 1;
+//    #512;
+//    RsRx = 0;
     
-    #128 RsRx = 0;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 1;
+//    #128 RsRx = 1;
+//    #128 RsRx = 0;
+//    #128 RsRx = 1;
+//    #128 RsRx = 0;
+//    #128 RsRx = 1;
+//    #128 RsRx = 0;
     
-    #128 RsRx = 1;
-    #512;
-    RsRx = 0;
+//    #128 RsRx = 1;
+//    #512;
+//    sw = 1;
+//    RsRx = 0;
     
-    #128 RsRx = 0;
-    #128 RsRx = 1;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 1;
+//    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 1;
+//    #128 RsRx = 0;
     
-    #128 RsRx = 1;
-    #512;
-    sw = 1;
-    RsRx = 0;
+//    #128 RsRx = 1;
+//    #512;
+//    RsRx = 0;
     
-    #128 RsRx = 0;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
+//    #128 RsRx = 1;
+//    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 1;
+//    #128 RsRx = 1;
+//    #128 RsRx = 0;
+//    #128 RsRx = 0;
     
-    #128 RsRx = 1;
-    #512;
-    RsRx = 0;
+//    #128 RsRx = 1;
+//    #512;
+//    RsRx = 0;
     
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 1;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
+//    #128 RsRx = 1;
+//    #128 RsRx = 1;
+//    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 0;
+//    #128 RsRx = 1;
+//    #128 RsRx = 0;
     
-    #128 RsRx = 1;
-    #512;
-    RsRx = 0;
-    
-    #128 RsRx = 1;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 0;
-    #128 RsRx = 1;
-    #128 RsRx = 0;
-    
-    #128 RsRx = 1;
-    #512;
+//    #128 RsRx = 1;
+//    #512;
     end
 
     // A = 10010100
