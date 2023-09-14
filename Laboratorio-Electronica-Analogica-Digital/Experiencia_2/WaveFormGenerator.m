bf1fad7fcf1cad12705c80353ba2f51f99b66d1d
Matias <mxti1415@gmail.com>
2023-09-14 12:58:22 -0300

    Añado generador de sinusoides matlab

---
+++ Laboratorio-Electronica-Analogica-Digital/Experiencia_2/WaveFormGenerator.m
@@ -0,0 +1,27 @@
+% Generamos la lista de muestras (1 ciclo)
+n = 256;
+i = 1;
+listaY = zeros(1, 256);
+listaX = zeros(1, 256);
+while i < n + 1
+    x = i - 1;
+    listaY(i) = int16(128*sin((2*pi/256)*x) + 128);
+    listaX(i) = x;
+    i = i + 1;  
+end
+
+% Probamos con n ciclos
+i = 0;
+n = 5;
+listaFinalY = [];
+listaFinalX = [];
+while i < n
+    auxY = listaFinalY;
+    auxX = listaFinalX;
+    listaFinalY = [auxY, listaY];
+    listaFinalX = [auxX, listaX + i*256];
+    i = i + 1;
+end
+
+plot(listaFinalX, listaFinalY);
+
